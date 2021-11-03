52e00452db6b459ab7b8fe5d935be6c56e0c067c
Greg Johnston <greg.johnston@gmail.com>
2021-11-03 11:38:53 -0400

    Module renaming for consistency

--- crates/lectionary/src/lectionaries/mod.rs
+++ crates/lectionary/src/lectionaries/mod.rs
@@ -1,5 +1,5 @@
+mod bcp1979_30_day_psalter;
 mod bcp1979_office;
-mod psalter_30day;
 
+pub use bcp1979_30_day_psalter::BCP1979_30_DAY_PSALTER;
 pub use bcp1979_office::BCP1979_DAILY_OFFICE_LECTIONARY;
-pub use psalter_30day::BCP1979_30_DAY_PSALTER;
