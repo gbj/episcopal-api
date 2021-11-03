2aaa398452141e4d54d69cf59ef5b5a5ceb15067
Greg Johnston <greg.johnston@gmail.com>
2021-11-03 13:05:49 -0400

    Add Psalms into API

--- examples/api/src/calendar.rs
+++ examples/api/src/calendar.rs
@@ -1,5 +1,7 @@
 use calendar::{Date, LiturgicalDay, BCP1979_CALENDAR};
-use lectionary::{Reading, BCP1979_DAILY_OFFICE_LECTIONARY};
+use lectionary::{
+    Reading, BCP1979_30_DAY_PSALTER, BCP1979_DAILY_OFFICE_LECTIONARY, BCP1979_DAILY_OFFICE_PSALTER,
+};
 use rocket::serde::{json::Json, Serialize};
 
 #[derive(Serialize)]
@@ -9,9 +11,16 @@ pub struct Readings {
 }
 
 #[derive(Serialize)]
+pub struct Psalms {
+    thirty_day: Vec<Reading>,
+    daily_office_lectionary: Readings,
+}
+
+#[derive(Serialize)]
 pub struct Summary {
     day: LiturgicalDay,
     daily_office_readings: Readings,
+    daily_office_psalms: Psalms,
 }
 
 #[get("/day?<year>&<month>&<day>&<evening>")]
@@ -28,8 +37,24 @@ pub fn day(year: u16, month: u8, day: u8, evening: bool) -> Json<Summary> {
                 .collect()
         }),
     };
+    let daily_office_psalms = Psalms {
+        thirty_day: BCP1979_30_DAY_PSALTER
+            .readings_by_day(&day.observed, &day)
+            .collect(),
+        daily_office_lectionary: Readings {
+            observed: BCP1979_DAILY_OFFICE_PSALTER
+                .readings_by_day(&day.observed, &day)
+                .collect(),
+            alternate: day.alternate.map(|alternate| {
+                BCP1979_DAILY_OFFICE_PSALTER
+                    .readings_by_day(&alternate, &day)
+                    .collect()
+            }),
+        },
+    };
     Json(Summary {
         day,
         daily_office_readings,
+        daily_office_psalms,
     })
 }
