d6f348fc862dec94fd98003c248ff9a78f17af39
Greg Johnston <greg.johnston@gmail.com>
2021-11-13 12:55:46 -0500

    Easy conversion functions

--- crates/reference-parser/src/lib.rs
+++ crates/reference-parser/src/lib.rs
@@ -8,3 +8,32 @@ pub use books::{book_name_to_book, Book};
 pub use query::BibleReferenceQuery;
 pub use range::BibleReferenceRange;
 pub use utils::parse_reference;
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+pub struct BibleReference {
+    pub ranges: Vec<BibleReferenceRange>,
+}
+
+impl From<&str> for BibleReference {
+    fn from(val: &str) -> Self {
+        BibleReference {
+            ranges: parse_reference(val),
+        }
+    }
+}
+
+impl From<String> for BibleReference {
+    fn from(val: String) -> Self {
+        BibleReference {
+            ranges: parse_reference(&val),
+        }
+    }
+}
+
+impl From<&String> for BibleReference {
+    fn from(val: &String) -> Self {
+        BibleReference {
+            ranges: parse_reference(val),
+        }
+    }
+}
