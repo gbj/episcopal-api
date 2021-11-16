70dbd3a0157bf654a76c6d27d996ea6b004c8ee8
Greg Johnston <greg.johnston@gmail.com>
2021-11-15 20:40:55 -0500

    Remove Words structure

--- crates/liturgy/src/words.rs
+++
@@ -1,9 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Clone, Debug, Hash, Eq, PartialEq, Serialize, Deserialize)]
-pub enum Words {
-    EnI(String),
-    EnII(String),
-    Es(String),
-    Fr(String),
-}
