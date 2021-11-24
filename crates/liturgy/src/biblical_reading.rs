ab38945ef778c2f62c1d8e4d0a9cfb3ef0619487
Greg Johnston <greg.johnston@gmail.com>
2021-11-24 13:16:37 -0500

    Merge Bible versions and liturgy versions

--- crates/liturgy/src/biblical_reading.rs
+++ crates/liturgy/src/biblical_reading.rs
@@ -5,18 +5,7 @@ use serde::{Deserialize, Serialize};
 #[derive(Clone, Debug, Hash, Eq, PartialEq, Serialize, Deserialize)]
 pub struct BiblicalReading {
     /// A citation for the book/chapters/verses included.
-    pub citation: Option<String>,
-    /// The Bible version or translation
-    pub version: BibleVersion,
+    pub citation: String,
     /// The text
     pub text: Vec<(BibleVerse, String)>,
-}
-
-/// Bible versions currently supported
-#[derive(Copy, Clone, Debug, Hash, Eq, PartialEq, Serialize, Deserialize)]
-pub enum BibleVersion {
-    NRSV,
-    ESV,
-    KJV,
-    CEB,
 }
--- crates/liturgy/src/version.rs
+++ crates/liturgy/src/version.rs
@@ -5,11 +5,24 @@ use serde::{Deserialize, Serialize};
 /// Different versions that a liturgical [Document](crate::Document) could be (e.g., Rite I, Rite II, EOW)
 #[derive(Copy, Clone, Debug, Hash, Eq, PartialEq, Serialize, Deserialize)]
 pub enum Version {
+    /// 1979 Book of Common Prayer
     BCP1979,
+    /// Rite I (traditional language in 1979 BCP)
     RiteI,
+    /// Rite II (contemporary language in 1979 BCP)
     RiteII,
+    /// Enriching Our Worship series
     EOW,
+    /// Expansive-language Eucharistic texts authorized by General Convention
     Expansive,
+    /// The New Revised Standard Version of the Bible
+    NRSV,
+    /// The English Standard Version of the Bible
+    ESV,
+    /// The Authorized Version (“King James Version”) of the Bible
+    KJV,
+    /// The Common English Bible
+    CEB,
 }
 
 impl Display for Version {
@@ -20,6 +33,10 @@ impl Display for Version {
             Version::RiteII => "Rite II",
             Version::EOW => "EOW",
             Version::Expansive => "Expansive",
+            Version::NRSV => "NRSV",
+            Version::ESV => "ESV",
+            Version::KJV => "KJV",
+            Version::CEB => "CEB",
         };
         write!(f, "{}", label)
     }
